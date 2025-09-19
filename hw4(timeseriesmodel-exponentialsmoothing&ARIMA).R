7b2f206a605b0fd4ca484e911330a2c98f1ed60d
Jongwook Eun <eunjongwook@gmail.com>
2025-09-19 15:25:35 +0900

    Update hw4(timeseriesmodel-exponentialsmoothing&ARIMA).R

--- hw4(timeseriesmodel-exponentialsmoothing&ARIMA).R
+++ hw4(timeseriesmodel-exponentialsmoothing&ARIMA).R
@@ -1,3 +1,8 @@
+### Question 7.2
+# Using the 20 years of daily high temperature data for Atlanta (July through October) from Question 6.2 (file temps.txt), build and use an exponential smoothing model to help make a judgment of whether the unofficial end of summer has gotten later over the 20 years.  (Part of the point of this assignment is for you to think about how you might use exponential smoothing to answer this question. Feel free to combine it with other models if you’d like to. There’s certainly more than one reasonable approach.) 
+# Note: in R, you can use either HoltWinters (simpler to use) or the smooth package’s es function (harder to use, but more general).  If you use es, the Holt-Winters model uses model=”AAM” in the function call (the first and second constants are used “A”dditively, and the third (seasonality) is used “M”ultiplicatively; the documentation doesn’t make that clear). 
+
+### Solution 7.2
 library(tidyverse)
 library(zoo)
 library(lubridate)
@@ -80,3 +85,4 @@ ggplot(by_year, aes(x = year, y = end_doy)) +
   geom_smooth(method = "lm", se = TRUE) +
   labs(title = "End of Summer DOY (Atlanta Jul–Oct)", x = "Year", y = "DOY") +
   theme_minimal()
+
