687d97c32d55d7d6d0968a1a25187b117a9765ec
Jongwook Eun <eunjongwook@gmail.com>
2025-10-22 15:33:51 +0900

    Update and rename Homework 9-1 (Question 12-2).R to hw9-1(designofexperiments).R

--- notebooks/Homework 9-1 (Question 12-2).R
+++
@@ -1,9 +0,0 @@
-library(FrF2)
-
-set.seed(123)
-design <- FrF2(nruns = 16, nfactors = 10, randomize = FALSE)
-
-names(design) <- c("Yard", "Solar", "Gar", "Pool", "Sch",
-                   "Kit", "Fire", "Smart", "Bal", "Base")
-
-design
